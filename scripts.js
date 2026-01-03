57da007ee31d1b89bdd447ad3f75bc5a88e403a1
toriisimone <toriisimone@gmail.com>
2026-01-03 14:43:05 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-placeholder.txt
+// Load categories from products.json
+fetch("products.json")
+  .then(response => response.json())
+  .then(data => {
+    const grid = document.querySelector(".ap-category-grid");
+
+    data.categories.forEach(cat => {
+      const card = document.createElement("div");
+      card.classList.add("ap-category-card");
+
+      card.innerHTML = `
+        <div class="ap-category-img" style="background-image: url('${cat.image}')"></div>
+        <div class="ap-category-name">${cat.name}</div>
+      `;
+
+      grid.appendChild(card);
+    });
+  });
